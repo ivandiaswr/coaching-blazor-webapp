65a1daac8f708f543eaa2768dd80528d0a9d3569
dybbuksbox <ivandiasworkrelated@proton.me>
2024-11-23 09:54:54 +0000

    new migration to update columns names more UI optimization

---
+++ DataAccessLayer/Migrations/20241122185053_UpdateColumnsNamesToCreatedAt.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateColumnsNamesToCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TimeStampInserted",
+                table: "EmailSubscriptions",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "TimeStampInserted",
+                table: "Contacts",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "EmailSubscriptions",
+                newName: "TimeStampInserted");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Contacts",
+                newName: "TimeStampInserted");
+        }
+    }
+}
