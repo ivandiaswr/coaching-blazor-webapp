8c09eee1df4de1e4aae3e5e49b6eef06ba58715c
dybbuksbox <ivandiasworkrelated@proton.me>
2025-04-26 18:45:13 +0100

    request session ui optimization in userdashboard

--- ModelLayer/Models/Enums/BookingType.cs
+++ ModelLayer/Models/Enums/BookingType.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ModelLayer.Models.Enums
 {
     public enum BookingType
     {
+        [Display(Name = "Single Session")]
         SingleSession,
+        [Display(Name = "Session Pack")]
         SessionPack,
+        [Display(Name = "Subscription")]
         Subscription
     }
 }
